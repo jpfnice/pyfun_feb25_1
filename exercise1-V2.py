5449bfa2743169ace74198b9849d05fe81cc6756
jpfnice <jpfnice@gmail.com>
2025-02-04 13:47:11 +0100

    Add files via upload

---
+++ exercise1-V2.py
@@ -0,0 +1,37 @@
+"""
+Write a Python script that prompts the user for a number and displays a message that does indicate if the number is odd or even.
+
+Note: do consider 0 has being neither odd nor even.
+
+After having tested a first number, the script should prompt the user for other numbers as long as he or she would like to.
+
+Note: to determine if a number is even you compute the remainder (via the %
+operator) of the division of this number by two, if it is zero, the number 
+is even.
+Unless the number is zero: zero is neither odd nor even
+
+"""
+
+while True:
+    
+    # Ctrl-1 to comment/uncomment:
+        
+    # nb=input("Please enter a number: ")
+    # nb=int(nb)
+    
+    nb=int(input("Please enter a number: "))
+
+    if nb == 0:
+        print(nb,"is neither even nor odd !")
+    elif nb % 2 == 0:
+        print(nb,"is even")
+    else:
+        print(nb,"is odd")
+    
+    response=input("Do you want to give me another number (Y/N) ? ")
+    if response == "N":
+        break
+    
+print("Bye bye!")
+    
+    
